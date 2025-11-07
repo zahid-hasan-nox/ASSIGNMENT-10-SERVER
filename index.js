4ea8609f28316c726e3c64643ef6503a065c06c8
zahid hasan nox <zahidvh0@gmail.com>
2025-11-07 18:12:56 +0600

    bug fix 3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 // ✅ CORS setup
 app.use(
